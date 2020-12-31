41099c5ed82278ba9699fd3efedd9fbb8d3027e1
Ingo Kleiber <ikleiber@googlemail.com>
2020-12-31 15:15:34 +0100

    Update install_windows.bat

--- install_windows.bat
+++ install_windows.bat
@@ -1,5 +1,5 @@
 mkdir backups\windows
 
 copy %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json backups\windows
-rm %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+del %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 mklink /H %USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json windows\settings.json
