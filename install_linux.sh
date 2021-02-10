f12f33eb498b5e9583593d7bcda62357818e2e90
Ingo Kleiber <ikleiber@googlemail.com>
2021-02-10 19:14:42 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -12,6 +12,7 @@ sudo mv ~/.config/nvim/init.vim $dotfiles_b/.config/nvim/init.vim
 sudo mv ~/.bashrc $dotfiles_b
 sudo mv ~/.bash_aliases $dotfiles_b
 sudo mv ~/.tmux.conf $dotfiles_b
+sudo mv ~/.nanorc $dotfiles_b
 
 # Create symlinks
 ln -sf $dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
@@ -20,3 +21,4 @@ ln -sf $dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf $dotfiles/.bashrc_misc ~/.bashrc_misc
 ln -sf $dotfiles/.bashrc_pentesting ~/.bashrc_pentesting
 ln -sf $dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf $dotfiles/.nanorc ~/.nanorc
