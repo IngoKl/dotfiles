d2241756cd32e2f79768800bc7152f5bc03b095b
Ingo Kleiber <ikleiber@googlemail.com>
2021-08-21 02:12:42 +0200

    Update crack_id_rsa.sh

--- scripts/crack_id_rsa.sh
+++ scripts/crack_id_rsa.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+id_rsa=$1
+
+echo $id_rsa
+
+if [ ! -f $id_rsa ]; then
+    echo "Cracking $1 using John"
+
+    python /usr/share/john/ssh2john.py $id_rsa > id_rsa.hash
+    john --wordlist=/usr/share/wordlists/rockyou.txt id_rsa.hash
+else
+    echo "./crack_id_rsa.sh /home/kali/.ssh/id_rsa"
+fi
